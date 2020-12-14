63fe25c2eb743d79d460092ec66ec0ccc949fbd7
sudojarvis <75030755+sudojarvis@users.noreply.github.com>
2020-12-15 05:27:43 +0530

    Create min-max_sum.cpp

---
+++ min-max_sum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[5];
+    for(int i=0;i<5;i++)
+    cin>>arr[i];
+    sort(arr,arr+5);
+    long min=0,max=0;
+    for(int j=0;j<4;j++)
+    min+=arr[j];
+    reverse(arr, arr+5);
+    for(int k=0;k<4;k++)
+    max+=arr[k];
+    cout<<min<<" "<<max;
+    return 0;    
+}
